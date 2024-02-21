7f49b670ad9321a9fa08f28d8d5c7cceab1ff845
Nwidag Kingsley <130276601+nwidagkingsley@users.noreply.github.com>
2024-02-21 22:45:43 +0100

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+print("hello, my name is answer bot, i welcome you to the question and answer chat bott")
+name = input("Hi,what is your name?")
+age = input("what is your age?")
+age = int(age)
+location = input("what is your location?")
+
+print("hello,", name, "you are", age, "years old and you live at", location)
